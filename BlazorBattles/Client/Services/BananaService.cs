1b65c50d36cd90b2a7d0159fb199141265b0d4ea
arwin-dev <arwin.dev@outlook.com>
2022-12-14 22:40:18 -0500

    section 14 completed

---
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorBattles.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+        public event Action OnChange;
+        public int Bananas { get; set; } = 1000;
+
+        public void EatBananas(int amount)
+        {
+            Bananas -= amount;
+            BananasChanged();
+        }
+
+        void BananasChanged() => OnChange.Invoke();
+    }
+}
---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBattles.Client.Services
+{
+    public interface IBananaService
+    {
+        event Action OnChange;
+        int Bananas { get; set; }
+        void EatBananas(int amount);
+    }
+}
