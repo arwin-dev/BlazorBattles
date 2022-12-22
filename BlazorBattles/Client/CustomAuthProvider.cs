857b4a5032fed0686fd644d27fa8958df3e645fc
arwin-dev <arwin.dev@outlook.com>
2022-12-21 19:15:42 -0500

    section 33 completed

---
+++ BlazorBattles/Client/CustomAuthProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorBattles.Client
+{
+    public class CustomAuthProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name,"Patrick")
+            }, "test authenitication type");
+
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+
+            
+        }
+    }
+}
--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -3,6 +3,7 @@ using BlazorBattles.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,5 +13,8 @@ builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IBananaService, BananaService>();
 builder.Services.AddScoped<IUnitService, UnitService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
